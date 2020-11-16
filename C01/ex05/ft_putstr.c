1ad3dbc04ceccc788b6cf46f7ad1df50a3b938bc
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-16 15:21:41 +0100

    C01-ex05-v01

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 15:18:21 by fspano            #+#    #+#             */
+/*   Updated: 2020/11/16 15:18:21 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(*str)
+	{	write(1, &str[i], 1)
+		i++;
+	}
+}
+
+int		main(void)
+{
+	char str[] = "Hello tout le monde";
+
+	ft_putstr(str);
+	return (0);
+}
