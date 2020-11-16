68ea7098af0312dcbc6f6efddd4c89e595d0764f
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-16 13:44:10 +0100

    ex 02

---
+++ C01/ex02/ft_swap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 13:43:43 by fspano            #+#    #+#             */
+/*   Updated: 2020/11/16 13:43:43 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void    ft_swap(int *a, int *b)
+{
+    int swp;
+
+    swp = a;
+    *a = b;
+    *b = swp;
+}
+
+int     main(void)
+{
+    int a;
+    int b;
+
+    a = 4;
+    b = 6;
+    printf("a = %d, b = %d", a, b);
+    ft_swap(a, b);
+    printf("a = %d, b = %d", a, b);
+}
