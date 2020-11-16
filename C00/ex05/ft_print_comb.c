4346a293c5c4380a2d65677288b5048779b3bcb4
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-16 13:07:10 +0100

    Update full C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/10 11:23:09 by fspano            #+#    #+#             */
+/*   Updated: 2020/10/10 16:01:24 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_write(char *number)
+{
+	write(1, &number[0], 1);
+	write(1, &number[1], 1);
+	write(1, &number[2], 1);
+	if (number[0] < '7')
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char number[3];
+
+	number[0] = '0';
+	number[1] = '1';
+	number[2] = '2';
+	while (number[0] < '8')
+	{
+		while (number[1] < '9')
+		{
+			while (number[2] <= '9')
+			{
+				ft_print_write(number);
+				number[2]++;
+			}
+			number[1]++;
+			number[2] = number[1] + 1;
+		}
+		number[1] = number[0] + 1;
+		number[0]++;
+	}
+}
