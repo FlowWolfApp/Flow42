d615f5fcd55c63faa42e0d433810d28c33731afa
Florian SPANO <fspano@student.42lyon.fr>
2020-11-16 16:08:30 +0100

    C01-ex07-v02

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:58:15 by fspano            #+#    #+#             */
-/*   Updated: 2020/11/16 15:58:15 by fspano           ###   ########lyon.fr   */
+/*   Updated: 2020/11/16 16:08:14 by fspano           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,35 @@ void	ft_rev_int_tab(int *tab, int size)
 	while (i < size / 2)
 	{
 		swp = tab[i];
-		tab[i] = tab[size - i];
-		tab[size - i] = swp;
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = swp;
 		i++;
 	}
 }
 
-int		maint(void)
+int		main(void)
 {
-	char tab[5];
+	int tab[5];
 	int i;
 	int size;
 
 	i = 0;
 	size = 5;
 	tab[0] = 1;
-	tab[1] = 1;
-	tab[2] = 1;
-	tab[3] = 1;
-	tab[4] = 1;
+	tab[1] = 2;
+	tab[2] = 3;
+	tab[3] = 4;
+	tab[4] = 5;
 	while (i < size)
 	{
-		printf("tab[i] = %d\n", i, tab[i]);
+		printf("tab[%d] = %d\n", i, tab[i]);
 		i++;
 	}
 	ft_rev_int_tab(tab, size);
 	i = 0;
 	while (i < size)
 	{
-		printf("tab[i] = %d\n", i, tab[i]);
+		printf("tab[%d] = %d\n", i, tab[i]);
 		i++;
 	}
 	return (0);
