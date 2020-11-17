c55732c621c76c39d244b0d71ac6c20f7e562b0a
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-17 10:44:30 +0100

    C04-ex02-v01

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,46 @@
+lea/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 10:38:05 by fspano            #+#    #+#             */
+/*   Updated: 2020/11/17 10:38:05 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr *= -1;
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + '0');
+}
+
+int		main(void)
+{
+	int a;
+
+	a = 458;
+	ft_putnbr(a);
+	return (0);
+}
