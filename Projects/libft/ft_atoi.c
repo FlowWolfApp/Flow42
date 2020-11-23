9769843607ab964c0be0457cc6f94927b59a29ac
Flow Dev <fspano@student.42lyon.fr>
2020-11-23 11:36:11 +0100

    update-atoi-v01

--- Projects/libft/ft_atoi.c
+++ Projects/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 10:47:38 by fspano            #+#    #+#             */
-/*   Updated: 2020/11/17 10:58:53 by fspano           ###   ########lyon.fr   */
+/*   Updated: 2020/11/23 11:35:47 by fspano           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int		ft_atoi(char *str)
 	i = 0;
 	negative = 1;
 	res = 0;
-	while(str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == ' '))
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == ' '))
 		i++;
-	while(str[i] == '-' || str[i] == '+')
+	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			negative = -negative;
 		i++;
 	}
-	while(str[i] && str[i] >= '0' && str[i] <= '9')
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + str[i] - '0';
 		i++;
