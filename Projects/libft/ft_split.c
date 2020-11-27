385f472ab5c8774f62d59e4917bffd2eade369a0
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-27 16:05:01 +0100

    P-libft_split

---
+++ Projects/libft/ft_split.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/27 15:36:42 by fspano            #+#    #+#             */
+/*   Updated: 2020/11/27 15:36:42 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// On compte le nombre de mots dans la chaine str
+static int	count_word(char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0:
+	count = 0;
+	while (str[i])
+	{
+		// Tant qu'on a un sep on avance dans la chaine
+		while (str[i] && (str[i] == c))
+			i++;
+		// Si on a pas un séparateur on compte +1  et on avance jusqu'au prochain séparateur
+		if (str[i] && !(str[i] == c))
+		{
+			count++;
+			while (str[i] && !(str[i] == c))
+				i++;
+		}
+		// On retourne le nombre de mots
+		return (count);
+	}
+}
+
+// On malloc la taille du mot
+static char	*malloc_word(char *str, char c)
+{
+	size_t	i;
+	size_t	size_word;
+	char	*word;
+
+	// On initialise la longueur
+	size_word = 0;
+	// Tant qu'on arrive pas à un séparateur on calcule la long du mot
+	while (str[size_word] && !(str[size_word] == c))
+		size_word++;
+	// On malloc de la longueur +1 de type char
+	if (!(word = (char *)malloc(sizeof(char) * (size_word + 1))))
+		return (NULL);
+	// On remplit le mot avec ce qui est dans la chaine
+	// On ajoute le \0 et on renvoit le mot
+	i = 0;
+	while (str[i] && !(str[i] == c))
+	{
+		word[i] = str[i];
+		i++
+	}
+	word[i] = '\0';
+	return (word);	
+}
+
+// On va séparer les parties de splits par le cara c dans un tab 2D
+char		**ft_split(char const *s, char c)
+{
+	// On init nos paramètres et un split en 2D qu'on malloc dans les 2 sesns
+	size_t	i;
+	size_t	index_word;
+	char	**split;
+	size_t	size_split;
+
+	i = 0;
+	index_word = 0;
+	// On compte le nombre de mots dans str
+	// On malloc de la taille de char * * nb de mots +1 pour le 0 en fin de tableau
+	size_split = count_word((char *)s, c);
+	if (!(split = (char **)malloc(sizeof(char *) * (size_split + 1))))
+		return (NULL);
+	// Sur toute la chaine
+	while (str[i])
+	{
+		// Tant qu'on est pas à la fin ou qu'on a pas un sep on avance
+		while (str[i] && str[i] == c)
+			i++;
+		// Quand on a un sep, on envoit jusqu'au prochain sep la chaine dans un word mallocké
+		if (str[i] && !(str[i] == c))
+		{
+			split[index_word] = malloc_word(&str[i]);
+			index_word++;
+			while (str[i] && !(str[i] == c))
+				i++;
+		}
+		split[index_word] = 0;
+		return (split);
+	}
+}
+
+int			main(void)
+{
+	char str_to_split[150] = "Voici la chaine à découper comme tu le sens copain";
+	char c = "c";
+
+	printf("chaine init : %s\ncarc : %c\n\nchaine finale : %s\n", str_to_split, c, ft_split(str_to_split, c));
+	return (0);
+}
