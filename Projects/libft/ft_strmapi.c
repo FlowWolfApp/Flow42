e586ad31faf34a6d92ec7cf31427b7dc277535bb
Flow Wolf <flow.wolf.app@gmail.com>
2020-11-28 11:09:49 +0100

    P-libft-_fd-files

---
+++ Projects/libft/ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fspano <fspano@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/28 09:07:39 by fspano            #+#    #+#             */
+/*   Updated: 2020/11/28 09:07:39 by fspano           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t			size;
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	size = ft_strlen(s);
+	if (!(str = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+
+}
